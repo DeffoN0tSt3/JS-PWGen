6ceb8e5a4ce56ae1380d11e1f068b5e590988f0a
n0tst3 <s@realinfosec.net>
2023-05-14 17:09:55 +0100

    Update main.js
    update to use moden Clipboard API and Cryptography API, use simplify getRandomSymbol, generatePassword

--- main.js
+++ main.js
@@ -1,102 +1,73 @@
-const getRandomLower = () => {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-const getRandomUpper = () => {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
-const getRandomNumber = () => {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
+const getRandomLower = () => String.fromCharCode(crypto.getRandomValues(new Uint8Array(1))[0] % 26 + 97);
+const getRandomUpper = () => String.fromCharCode(crypto.getRandomValues(new Uint8Array(1))[0] % 26 + 65);
+const getRandomNumber = () => String.fromCharCode(crypto.getRandomValues(new Uint8Array(1))[0] % 10 + 48);
 
 const getRandomSymbol = () => {
-	// return String.fromCharCode(Math.floor(Math.random() * 16) + 33)
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)]
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[crypto.getRandomValues(new Uint8Array(1))[0] % symbols.length];
 }
 
 const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
 }
 
-const resultEl = document.querySelector('#result')
-const lengthEl = document.querySelector('#length')
-const uppercaseEl = document.querySelector('#uppercase')
-const lowercaseEl = document.querySelector('#lowercase')
-const numbersEl = document.querySelector('#numbers')
-const symbolsEl = document.querySelector('#symbols')
-const generateEl = document.querySelector('#generate')
-const clipboardEl = document.querySelector('#clipboard')
+const resultEl = document.querySelector('#result');
+const lengthEl = document.querySelector('#length');
+const uppercaseEl = document.querySelector('#uppercase');
+const lowercaseEl = document.querySelector('#lowercase');
+const numbersEl = document.querySelector('#numbers');
+const symbolsEl = document.querySelector('#symbols');
+const generateEl = document.querySelector('#generate');
+const clipboardEl = document.querySelector('#clipboard');
 
 const generatePassword = (lower, upper, number, symbol, length) => {
-	let newPassword = ''
-	const typesCount = lower + upper + number + symbol
-	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
-		item => Object.values(item)[0]
-	)
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    item => Object.values(item)[0]
+  );
 
-	if (typesCount === 0) {
-		return ''
-	}
+  if (typesCount === 0) return '';
 
-	for (let i = 0; i < length; i += typesCount) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0]
-			newPassword += randomFunc[funcName]()
-		})
-	}
+  const defaultLength = typesArr.length * 8;
+  const finalLength = Math.max(length, defaultLength);
 
-	const finalPassword = newPassword.slice(0, length)
-	return finalPassword
+  let newPassword = Array(finalLength).fill(null).flatMap(() => typesArr)
+    .map(type => {
+      const funcName = Object.keys(type)[0];
+      return randomFunc[funcName]();
+    });
+
+  return newPassword.join('').slice(0, length);
 }
 
 generateEl.addEventListener('click', () => {
-	const length = +lengthEl.value
-	const hasLower = lowercaseEl.checked
-	const hasUpper = uppercaseEl.checked
-	const hasNumber = numbersEl.checked
-	const hasSymbol = symbolsEl.checked
-	resultEl.innerText = generatePassword(
-		hasLower,
-		hasUpper,
-		hasNumber,
-		hasSymbol,
-		length
-	)
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
 })
 
-/* clipboardEl.addEventListener('click', () => {
-	const textarea = document.createElement('textarea')
-	const password = resultEl.innerText
+clipboardEl.addEventListener('click', async () => {
+  const password = resultEl.innerText;
 
-	if (!password) {
-		return
-	}
+  if (!password) return;
 
-	textarea.value = password
-	document.body.appendChild(textarea)
-	textarea.select()
-	document.execCommand('copy')
-	textarea.remove()
-	alert('Password copied to clipboard')
-}) */
-
-clipboardEl.addEventListener('click', event => {
-	const range = document.createRange()
-	range.selectNode(resultEl)
-	window.getSelection().addRange(range)
-
-	try {
-		const successful = document.execCommand('copy')
-		const msg = successful ? 'successful' : 'unsuccessful'
-		// console.log('Copied text: ' + msg)
-	} catch (err) {
-		// console.log('Error copy text');
-	}
-
-	window.getSelection().removeAllRanges()
+  try {
+    await navigator.clipboard.writeText(password);
+    alert('Password copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
 })
